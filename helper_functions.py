Fixed voxel size calculation for 3D phasing and resolution axis calculation in all files. Added separat function in helper_functions file.

helper_functions.py
@@ -124,6 +124,23 @@ def write_text(txt):
     stderr.write(txt)
 
 
+def calc_voxel_size(lambda_photon, d_detector, s_pixel, dim):
+    """Calculate voxel size in nm^-1 with the crystallographers' convention assuming cubic array."""
+    pixel_num = dim - dim//2
+    min_angle = np.arctan(s_pixel / d_detector)
+    max_angle = np.arctan(np.sqrt((pixel_num * s_pixel)**2 + (pixel_num * s_pixel)**2) / d_detector)
+    
+    ewald_rad = d_detector / s_pixel
+    q_max = (2. * np.sin(0.5 * max_angle)) / lambda_photon
+    q_sep = ((2. * np.sin(0.5 * min_angle)) / lambda_photon) * (d_detector / ewald_rad / s_pixel)
+    fov = 1 / q_sep
+    
+    voxel_size = fov / dim
+    half_p_res = 0.5 / q_max
+    max_p_res = 2 * half_p_res
+    return voxel_size, half_p_res, max_p_res
+
+    
 # https://stackoverflow.com/questions/42464334/find-the-intersection-of-two-curves-given-by-x-y-data-with-high-precision-in
 def interpolated_intercepts(x, y1, y2):
     """Find the intercepts of two curves, given by the same x data"""
@@ -228,112 +245,3 @@ def det_dist_solver(det_dist_0, dimX, pixel_size, n_iters=1):
         d = A / math.tan(2.0 * math.asin(x))
     return d
 
-
-def numpy_array_to_image(img,msk=None):
-    import spimage
-    s = img.shape
-    d = len(list(s))
-    if d == 3:
-        sp_img = spimage.sp_image_alloc(s[2],s[1],s[0])
-    else:
-        sp_img = spimage.sp_image_alloc(s[1],s[0],1)
-    sp_img.image[:] = img[:]
-    if msk is not None:
-        sp_img.mask[:] = msk[:]
-    return sp_img
-
-
-def prtf(images_rs,supports,translate=True,enantio=True,full_out=False):
-    """
-    NOTE: For using the enantio option, the images need to be centered in fourier space (no phase ramp in real space)
-    """
-    import spimage
-    S = images_rs.shape
-    s = list(S)
-    N = s.pop(0)
-    s = tuple(s)
-
-    image0_rs = images_rs[0]
-    image0_fs = numpy.fft.fftn(image0_rs)
-
-    sp_image0_rs = numpy_array_to_image(image0_rs,supports[0])
-    sp_image0_fs = numpy_array_to_image(image0_fs)
-
-    sp_amp_fs = spimage.sp_image_duplicate(sp_image0_fs,spimage.SP_COPY_ALL)
-    spimage.sp_image_dephase(sp_amp_fs)
-
-    spimage.sp_image_free(sp_image0_rs)
-    spimage.sp_image_free(sp_image0_fs)
-
-    sum_fs = image0_fs.copy()
-    sum_fs[abs(sum_fs) > 0.] /= abs(sum_fs[abs(sum_fs) > 0.])
-
-    sp_sum_fs = numpy_array_to_image(sum_fs)
-
-    zeros = numpy.zeros(shape=s,dtype="int")
-    zeros[abs(sum_fs) <= 0.] = 1
-
-    sp_avg_img = numpy_array_to_image(image0_rs)
-    avg_msk = numpy.zeros(shape=s,dtype="float")
-    
-    images_rs_super = numpy.zeros(shape=S,dtype="complex64")
-    images_rs_super[0,:] = image0_rs[:]
-    masks_rs_super = numpy.zeros(shape=S,dtype="bool")
-    masks_rs_super[0,:] = supports[0,:]
-
-    for i,img,sup in zip(range(1,N),images_rs[1:],supports[1:]):
-        # Initialize image
-        sp_img = numpy_array_to_image(img,sup)
-
-        # Translate and enantio matching
-        if translate:
-            spimage.sp_image_superimpose(sp_avg_img,sp_img, spimage.SpEnantiomorph if enantio else 0)
-            spimage.sp_image_phase_match(sp_avg_img,sp_img,2)
-        spimage.sp_image_add(sp_avg_img,sp_img)
-
-        if sp_img.mask.sum() > 0:
-            avg_msk[sp_img.mask == 0] += 1
-
-        # Cache image and support
-        images_rs_super[i,:] = sp_img.image[:]
-        masks_rs_super[i,:] = sp_img.mask[:]
-        
-        # Add amplitudes
-        sp_tmp = spimage.sp_image_fftw3(sp_img)
-        sp_tmpamp = spimage.sp_image_duplicate(sp_tmp,spimage.SP_COPY_ALL)
-        spimage.sp_image_dephase(sp_tmpamp);
-        spimage.sp_image_add(sp_amp_fs,sp_tmpamp)
-        
-        # Count zeros
-        positive = abs(sp_tmp.image) > 0.
-        sp_tmp.image[positive] /= abs(sp_tmp.image)[positive]
-        zeros += (positive == False)
-        
-        spimage.sp_image_add(sp_sum_fs,sp_tmp)
-        
-        spimage.sp_image_free(sp_img)
-        spimage.sp_image_free(sp_tmp)
-        spimage.sp_image_free(sp_tmpamp)
-  
-    sp_prtf = spimage.sp_image_duplicate(sp_sum_fs,spimage.SP_COPY_DATA|spimage.SP_COPY_MASK)
-    sp_prtf.image[:] /= N
-    sp_prtf.image[zeros > 0] = 0.
-    spimage.sp_image_dephase(sp_prtf)
-
-    avg_img = sp_avg_img.image[:].copy()
-    avg_sup = avg_msk > 0
-    prtf = abs(sp_prtf.image[:]).copy()
-    prtf = numpy.fft.fftshift(prtf)
-
-    for sp_i in [sp_prtf,sp_avg_img,sp_amp_fs,sp_sum_fs]:
-        spimage.sp_image_free(sp_i)  
-      
-    out = {}
-    out["prtf"] = prtf
-    out["super_image"] = avg_img
-    if full_out:
-        out["prtf_r"] = spimage.radial(prtf,f=numpy.mean,shell_thickness=1.0) 
-        out["super_mask"] = avg_sup 
-        out["images"] = images_rs_super
-        out["masks"] = masks_rs_super
-    return out

spsim_3D_phasing_uv_env_prot_wat_2.py
@@ -26,7 +26,7 @@ e = constants.elementary_charge
 h = constants.Planck
 
 subBg = True
-vnum = "2"
+vnum = "1"
     
 emc_file = "emc/protein_water_ds_4x_0001/data_1M/prot_wat_2/output_130.h5"
 with h5py.File(emc_file, "r") as f_ptr:
@@ -58,35 +58,23 @@ sType = file.split(sep="/")[2]
 pType = "emc_" + file.split(sep="/")[3]
 print(f"Phasing {pType} model ({npats} patterns)!")
 
-e_photon_eV = 9000
-lambda_photon = (h * c) / (e_photon_eV * e)
+lambda_photon = (h * c) / (9000 * e)
 d_detector = 0.5
 s_pixel = 800e-6
-
 dimX = frame.shape[0]
-dimY = frame.shape[1]
-dimZ = frame.shape[2]
-
-pixel_num = dimX - dimX // 2
-theta_pixel = 0.5 * np.arctan((pixel_num * s_pixel) / d_detector)
-center_to_corner = np.sqrt((pixel_num * s_pixel) ** 2 + (pixel_num * s_pixel) ** 2)
-theta_max = 0.5 * np.arctan(center_to_corner / d_detector)
-
-resolution = lambda_photon / (2.0 * np.sin(theta_pixel))
-resolution_max = lambda_photon / (2.0 * np.sin(theta_max))
-voxel_size = 0.5 * resolution
+voxel_size, _ , _ = calc_voxel_size(lambda_photon, d_detector, s_pixel, dimX)
 
 R_part = (15e-9) / 2
 R_particle_vox = R_part / voxel_size
 
 volume_fraction_sphere = (((4/3) * pi) * (R_particle_vox**3)) / (
-    dimX * dimY * dimZ
+    dimX * dimX * dimX
 )
 
 support_sphere = sphere_idx(
     shape=frame.shape,
     radius=R_particle_vox,
-    position=(dimX // 2, dimY // 2, dimZ // 2),
+    position=(dimX // 2, dimX // 2, dimX // 2),
 )
 
 vol_frac = volume_fraction_sphere
@@ -226,8 +214,8 @@ with open(save_location + "phasing_params.txt", "w") as handle:
     handle.write(f"File to phase: {file}\n")
     handle.write(f"EMC fraction good : {frac_emc_good}\n")
     handle.write(f"Fourier mask: {fourier_mask}\n")
-    handle.write(f"Array size: ({dimX}, {dimY}, {dimZ})\n")
-    handle.write(f"Voxel size: {voxel_size*1e9} nm\n")
+    handle.write(f"Array size: ({dimX}, {dimX}, {dimX})\n")
+    handle.write(f"Voxel size: {voxel_size*1e10} Å\n")
     handle.write(f"Volume percentage initial and i_frac: {volume_i*100}% and {i_frac}\n")
     handle.write(f"Volume percentage final and f_frac: {volume_f*100}% and {f_frac}\n")
     handle.write(f"Number of voxels in support: {supp_phase.sum()}\n")